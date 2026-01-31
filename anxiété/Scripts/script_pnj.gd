196fac653f2e25b0670742af03cf11273d2c6df2
FlorianLesieur <florian.lesieur@telecom-sudparis.eu>
2026-01-31 17:24:37 +0100

    max_distance et max_angle pour la vue

--- anxiété/Scripts/script_pnj.gd
+++ anxiété/Scripts/script_pnj.gd
@@ -6,13 +6,22 @@ var direction
 
 @onready var raycast = $RayCast2D
 @export var goal : Node2D
+@export var distance_max_vue = 200000
 
 func ray_cast():
 	if goal == null:
 		return false
 	raycast.target_position = raycast.to_local(goal.global_position)
 	raycast.force_raycast_update()
-	if raycast.is_colliding():
+	#print((goal.global_position - self.position).normalized())
+	#print(direction.normalized())
+	if (goal.global_position - self.position).dot(goal.global_position - self.position) > distance_max_vue:
+		#print("trop loin")
+		return false
+	if (goal.global_position - self.position).normalized().dot(direction.normalized()) < 0.75:
+		#print("angle trop grand")
+		return false
+	elif raycast.is_colliding():
 		return raycast.get_collider() == goal
 	
 	return false
@@ -27,4 +36,5 @@ func _physics_process(delta: float) -> void:
 	trajet.progress += SPEED*delta
 	direction = trajet.position - self.position
 	velocity = SPEED*direction.normalized()
+	look_at(trajet.position)
 	move_and_slide()
