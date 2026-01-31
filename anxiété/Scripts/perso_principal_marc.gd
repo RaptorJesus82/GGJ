73975c33ea143e3e517f320445ac1f0a0272aa65
Raptorjesus42 <mbetous82@gmail.com>
2026-01-31 20:30:36 +0100

    le chat il marche wtf

--- anxiété/Scripts/perso_principal_marc.gd
+++ anxiété/Scripts/perso_principal_marc.gd
@@ -1,36 +1,23 @@
-extends CharacterBody2D
+extends RigidBody2D
 
+const FORCE = 12000.0
+const TORQUE_FORCE = 700000.0
+const MAX_SPEED = 400.0
 
-const SPEED = 180
-const inertia = 1
-const rotationspeed = 0.006
+func _ready():
+	linear_damp = 6.0
+	angular_damp = 6.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if Input.is_action_pressed("ui_right") :
-		rotation = lerp_angle(rotation, 0, rotationspeed)
-		velocity.y = move_toward(velocity.y, SPEED*sin(rotation), SPEED/inertia)
-		velocity.x = move_toward(velocity.x, cos(rotation)*SPEED, SPEED/inertia)
-	elif Input.is_action_pressed("ui_left") :
+	if Input.is_action_pressed("ui_left"):
+		apply_torque(-TORQUE_FORCE)
 
-		rotation = lerp_angle(rotation, PI, rotationspeed)
+	if Input.is_action_pressed("ui_right"):
+		apply_torque(TORQUE_FORCE)
 
-		velocity.y = move_toward(velocity.y, SPEED*sin(rotation), SPEED/inertia)
-		velocity.x = move_toward(velocity.x, cos(rotation)*SPEED, SPEED/inertia)
-	elif Input.is_action_pressed("ui_up") :
-		rotation = lerp_angle(rotation, -PI/2, rotationspeed)
-		velocity.y = move_toward(velocity.y, SPEED*sin(rotation), SPEED/inertia)
-		velocity.x = move_toward(velocity.x, cos(rotation)*SPEED, SPEED/inertia)
-	elif Input.is_action_pressed("ui_down") :
-		rotation = lerp_angle(rotation, PI/2+0.001, rotationspeed)
-		velocity.y = move_toward(velocity.y, SPEED*sin(rotation), SPEED/inertia)
-		velocity.x = move_toward(velocity.x, cos(rotation)*SPEED, SPEED/inertia)
-		
-
-	else :
-		velocity.x = move_toward(velocity.x, 0, SPEED/inertia)
-		velocity.y = move_toward(velocity.x, 0, SPEED/inertia)
-
-	move_and_slide()
+	# --- Avancer ---
+	if Input.is_action_pressed("ui_up"):
+		var forward = Vector2.RIGHT.rotated(rotation)
+		if linear_velocity.length() < MAX_SPEED:
+			apply_central_force(forward * FORCE)
