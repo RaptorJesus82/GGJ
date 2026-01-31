715bcd53510e634c22298525524552975afb70c2
FlorianLesieur <florian.lesieur@telecom-sudparis.eu>
2026-01-31 13:21:39 +0100

    ajout du script de mouvement dans florian1

--- anxiété/Scripts/script_pnj.gd
+++ anxiété/Scripts/script_pnj.gd
@@ -1,24 +1,7 @@
 extends CharacterBody2D
-
-var speed = 300
-
-func _physics_process(delta):
-	var input_dir = Vector2.ZERO
-
-	if Input.is_action_pressed("ui_left"):
-		input_dir.x -= 1
-
-	if Input.is_action_pressed("ui_right"):
-		input_dir.x += 1
-
-	if Input.is_action_pressed("ui_up"):
-		input_dir.y -= 1
-
-	if Input.is_action_pressed("ui_down"):
-		input_dir.y += 1
-
-	velocity = input_dir.normalized() * speed
-	move_and_collide(velocity * delta)
+class_name ennemy_with_sight
+@export var SPEED = 300.0
+@export var trajet : PathFollow2D
 
 @onready var raycast = $RayCast2D
 @export var goal : Node2D
@@ -34,6 +17,8 @@ func ray_cast():
 	return false
 
 func _process(delta):
+	trajet.progress += SPEED*delta
+	self.position=trajet.position
 	if ray_cast():
 		print("Player visible!")
 	else:
