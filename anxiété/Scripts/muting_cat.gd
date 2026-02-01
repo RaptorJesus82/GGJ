07335e1b84e26d5eea153f41137ccb1401a26f80
timotheejonquay <timothee.jonquay@telecom-sudparis.eu>
2026-02-01 10:28:12 +0100

    envoi de signaux de mutation aux ennemis

--- anxiété/Scripts/muting_cat.gd
+++ anxiété/Scripts/muting_cat.gd
@@ -3,8 +3,10 @@ extends Node2D
 @export var mutations : Array[Node2D]
 var remainingMutation : Array[Node2D]
 var currentMutation : Array[Node2D]
-var stress : int = 1
+var stress : float =1.5
 var mut
+signal muted_up
+signal muted_down
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for mut in mutations:
@@ -14,11 +16,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if stress > len(currentMutation):
+	if int(stress) > len(currentMutation):
 		mut = remainingMutation.pop_at(remainingMutation.find(remainingMutation.pick_random()))
 		mut.show()
 		currentMutation.append(mut)
-	elif stress < len(currentMutation) :
+		muted_up.emit(mut)
+	elif int(stress) < len(currentMutation) :
 		mut = currentMutation.pop_at(currentMutation.find(currentMutation.pick_random()))
 		mut.hide()
 		remainingMutation.append(mut)
+		muted_down.emit(mut)
