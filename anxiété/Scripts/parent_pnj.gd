3994c047c34fe966496dd7e2973b1dc80aa6ed7e
timotheejonquay <timothee.jonquay@telecom-sudparis.eu>
2026-02-01 13:54:01 +0100

    possibilté d'équilibrer

--- anxiété/Scripts/parent_pnj.gd
+++ anxiété/Scripts/parent_pnj.gd
@@ -1,11 +1,29 @@
 extends Node2D
 @export var path : PathFollow2D
+@export var speedPath : float
+@export var distance_max_vue = 200000
+@export var TORQUE_FORCE = 600000.0
+@export var MAX_SPEED = 400.0
+@export var FORCE = 12000.0
+@export var angle_dir = 0.5
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var node = $Corps
-	node.trajet = path
+	node.trajet = self.path
+	node.SPEED = self.speedPath
+	node.distance_max_vue = self.distance_max_vue
+	node.TORQUE_FORCE = self.TORQUE_FORCE
+	node.MAX_SPEED = self.MAX_SPEED
+	node.FORCE = self.FORCE
+	node.angle_dir = self.angle_dir
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called when the node enters the scene tree for the first time.
 func _process(delta: float) -> void:
-	pass
+	var node = $Corps
+	node.trajet = self.path
+	node.SPEED = self.speedPath
+	node.distance_max_vue = self.distance_max_vue
+	node.TORQUE_FORCE = self.TORQUE_FORCE
+	node.MAX_SPEED = self.MAX_SPEED
+	node.FORCE = self.FORCE
+	node.angle_dir = self.angle_dir
